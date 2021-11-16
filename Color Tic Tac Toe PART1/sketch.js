e882233e91eb201195ea8006a33ef5bd809634d4
unknown <raveen.anand@whitehatjr.com>
2021-11-16 19:52:14 +0530

    v2

--- Color Tic Tac Toe PART1/sketch.js
+++ Color Tic Tac Toe PART1/sketch.js
@@ -1,32 +1,20 @@
 var turn=0
 function setup() {
-    //size of the canvas to draw on
     createCanvas(innerWidth,innerHeight); 
-    // Set the background to white, and move the line to setup();
     background("white");
-
-    fill("#404040");
+    fill("black");
     rect(0, 0, width, 50);
-
     textAlign(CENTER);
     fill("white");
     textSize(18);
     text("Color Tic Tac Toe", width / 2, 33);
-    
 }
-
 
 function draw()
 {
-// Call Create Row Function with different y values for each row
-//Row 1: y = 150 
-createRow(150);
-
-//Row 2: y = 250
- createRow(250);
-
- //Row 3: y = 350
- createRow(350);  
+    createRow(150);
+    createRow(250);
+    createRow(350);  
 }
 
 function createRow(y)
